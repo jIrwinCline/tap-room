5fc0b4fb45b6ccc2e7453a999dfd705e857336a3
jirwincline <jakobinwins@gmail.com>
2019-09-07 15:18:46 -0700

    running into weird error... ERROR in ./src/index.js 8:16

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import HeaderNavbar from './HeaderNavbar.js'
+import headerPic from '../public/headerPic.jpg'
 
 
 
 export default function Header() {
   return (
     <div>
+      <img src={headerPic} alt='Header Picture'/>
       <HeaderNavbar/>
     </div>
   );

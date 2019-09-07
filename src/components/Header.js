1cb5d35ff4c37c1a9b0699cffaf882f7ff4b7d53
jirwincline <jakobinwins@gmail.com>
2019-09-07 14:39:48 -0700

    add components for header footer and body

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HeaderNavbar from './HeaderNavbar.js'
+
+
+
+export default function Header() {
+  return (
+    <div>
+      <HeaderNavbar/>
+    </div>
+  );
+}
---
+++ src/components/HeaderNavbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+export default function HeaderNavbar() {
+  return (
+    <div>
+
+    </div>
+  );
+}
